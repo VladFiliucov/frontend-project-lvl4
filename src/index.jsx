83215f3490cb0dc5c9dfb7e55336aac9c10d45c8
Vlad Filiucov <filiucov@mail.ru>
2021-08-07 14:09:20 +0300

    make i18n asynchronous

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,24 @@
 // @ts-check
 import React from 'react';
+import { I18nextProvider } from 'react-i18next';
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 
 import App from './components/App.jsx';
 
 import '../assets/application.scss';
-import './initializers/i18n';
+import generateI18NInstance from './initializers/i18n';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-export default async (socketClient) => <App socketClient={socketClient} />;
+export default async (socketClient) => {
+  const i18nInstance = await generateI18NInstance();
+
+  return (
+    <I18nextProvider i18n={i18nInstance}>
+      <App socketClient={socketClient} />
+    </I18nextProvider>
+  );
+};
--- src/initializers/i18n.js
+++ src/initializers/i18n.js
@@ -1,141 +1,137 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
-i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  // .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    lng: 'ru',
-    debug: false,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-      en: {
-        translation: {
-          siteName: 'Chat',
-          signup: 'Sign up',
-          signout: 'Sign out',
-          login: 'Sign in',
-          incorrectCredentials: 'Incorrect username or password',
-          dontHaveAccount: "Don't have an account? - ",
-          signin: {
-            required: 'This field is required',
-          },
-          signupPage: {
-            headline: 'Registration',
-            form: {
-              cta: 'Sign up',
-              fields: {
-                username: 'Username',
-                password: 'Password',
-                passwordConfirmation: 'Password confirmation',
+export default async () => {
+  const instance = i18n.createInstance();
+
+  await instance
+    .init({
+      lng: 'ru',
+      debug: false,
+      fallbackLng: 'en',
+      interpolation: {
+        escapeValue: false, // not needed for react as it escapes by default
+      },
+      resources: {
+        en: {
+          translation: {
+            siteName: 'Chat',
+            signup: 'Sign up',
+            signout: 'Sign out',
+            login: 'Sign in',
+            incorrectCredentials: 'Incorrect username or password',
+            dontHaveAccount: "Don't have an account? - ",
+            signin: {
+              required: 'This field is required',
+            },
+            signupPage: {
+              headline: 'Registration',
+              form: {
+                cta: 'Sign up',
+                fields: {
+                  username: 'Username',
+                  password: 'Password',
+                  passwordConfirmation: 'Password confirmation',
+                },
+                errors: {
+                  required: 'Required field',
+                  passwordMatch: 'Passwords must match',
+                  usernameLength: 'From 3 to 20 characters',
+                  passwordLength: 'Minimum 6 characters',
+                  userExists: 'Username already taken',
+                },
               },
-              errors: {
-                required: 'Required field',
-                passwordMatch: 'Passwords must match',
-                usernameLength: 'From 3 to 20 characters',
-                passwordLength: 'Minimum 6 characters',
-                userExists: 'Username already taken',
+            },
+            chatPage: {
+              headline: 'Channels',
+              messages: 'Messages',
+              form: {
+                title: 'Add new channel',
+                actions: {
+                  cancel: 'Cancel',
+                  submit: 'Add',
+                },
+                errors: {
+                  required: 'Required field',
+                  channelExists: 'Must be uniq',
+                },
+              },
+              messageForm: {
+                errors: {
+                  required: 'Required field',
+                },
+                actions: {
+                  submit: 'Send',
+                },
               },
             },
           },
-          chatPage: {
-            headline: 'Channels',
-            messages: 'Messages',
-            form: {
-              title: 'Add new channel',
-              actions: {
-                cancel: 'Cancel',
-                submit: 'Add',
-              },
-              errors: {
-                required: 'Required field',
-                channelExists: 'Must be uniq',
+        },
+        ru: {
+          translation: {
+            siteName: 'Hexlet Chat',
+            signup: 'Регистрация',
+            signout: 'Выйти',
+            login: 'Войти',
+            dontHaveAccount: 'Нет аккаунта? - ',
+            signinPage: {
+              form: {
+                cta: 'Зерегестрироваться',
+                fields: {
+                  username: 'Ваш ник',
+                  password: 'Пароль',
+                  passwordConfirmation: 'Подтвердите пароль',
+                },
+                errors: {
+                  required: 'Обязательное поле',
+                  incorrectCredentials: 'Неверные имя пользователя или пароль',
+                },
               },
             },
-            messageForm: {
-              errors: {
-                required: 'Required field',
+            signupPage: {
+              headline: 'Регистрация',
+              form: {
+                cta: 'Зарегистрироваться',
+                fields: {
+                  username: 'Имя пользователя',
+                  password: 'Пароль',
+                  passwordConfirmation: 'Подтвердите пароль',
+                },
+                errors: {
+                  required: 'Обязательное поле',
+                  passwordMatch: 'Пароли должны совпадать',
+                  usernameLength: 'От 3 до 20 символов',
+                  passwordLength: 'Не менее 6 символов',
+                  userExists: 'Такой пользователь уже существует',
+                },
               },
-              actions: {
-                submit: 'Send',
+            },
+            chatPage: {
+              headline: 'Каналы',
+              messages: 'Сообщения',
+              form: {
+                title: 'Добавить канал',
+                actions: {
+                  cancel: 'Отменить',
+                  submit: 'Отправить',
+                },
+                errors: {
+                  required: 'Обязательное поле',
+                  channelExists: 'Должно быть уникальным',
+                },
+              },
+              messageForm: {
+                errors: {
+                  required: 'Обязательное поле',
+                },
+                actions: {
+                  submit: 'Отправить',
+                },
               },
             },
           },
         },
       },
-      ru: {
-        translation: {
-          siteName: 'Hexlet Chat',
-          signup: 'Регистрация',
-          signout: 'Выйти',
-          login: 'Войти',
-          dontHaveAccount: 'Нет аккаунта? - ',
-          signinPage: {
-            form: {
-              cta: 'Зерегестрироваться',
-              fields: {
-                username: 'Ваш ник',
-                password: 'Пароль',
-                passwordConfirmation: 'Подтвердите пароль',
-              },
-              errors: {
-                required: 'Обязательное поле',
-                incorrectCredentials: 'Неверные имя пользователя или пароль',
-              },
-            },
-          },
-          signupPage: {
-            headline: 'Регистрация',
-            form: {
-              cta: 'Зарегистрироваться',
-              fields: {
-                username: 'Имя пользователя',
-                password: 'Пароль',
-                passwordConfirmation: 'Подтвердите пароль',
-              },
-              errors: {
-                required: 'Обязательное поле',
-                passwordMatch: 'Пароли должны совпадать',
-                usernameLength: 'От 3 до 20 символов',
-                passwordLength: 'Не менее 6 символов',
-                userExists: 'Такой пользователь уже существует',
-              },
-            },
-          },
-          chatPage: {
-            headline: 'Каналы',
-            messages: 'Сообщения',
-            form: {
-              title: 'Добавить канал',
-              actions: {
-                cancel: 'Отменить',
-                submit: 'Отправить',
-              },
-              errors: {
-                required: 'Обязательное поле',
-                channelExists: 'Должно быть уникальным',
-              },
-            },
-            messageForm: {
-              errors: {
-                required: 'Обязательное поле',
-              },
-              actions: {
-                submit: 'Отправить',
-              },
-            },
-          },
-        },
-      },
-    },
-  });
+    });
 
-export default i18n;
+  return instance;
+};
