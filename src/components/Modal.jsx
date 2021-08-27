93031ae44ccc7a5afddf07c73622a765e2ff5cdd
Vlad Filiucov <filiucov@mail.ru>
2021-08-27 13:42:20 +0300

    refactor create channel modal to use dynamc dispatch

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,44 @@
 import React, { createRef } from 'react';
-import { useSelector } from 'react-redux';
-import CreateChannel from './CreateChannel';
+import { Modal } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import CreateChannelForm from './CreateChannelForm';
+import { hideModal } from '../store/modalSlice';
 
 const ModalContents = () => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
   const newChannelInputRef = createRef(null);
-  const modalOpened = useSelector((state) => state.modal.isOpened);
+  const { isOpened, type, options } = useSelector((state) => state.modal);
 
-  return <CreateChannel newChannelInputRef={newChannelInputRef} isOpened={modalOpened} />;
+  const handleClose = () => {
+    dispatch(hideModal());
+  };
+
+  const modals = {
+    NewChannelModal: {
+      modalTitle: t('chatPage.form.title'),
+      modalBodyComponent: CreateChannelForm,
+      modalProps: {
+        newChannelInputRef,
+      },
+    },
+  };
+
+  const currentModal = modals[type];
+
+  if (!currentModal) return null;
+
+  return (
+    <Modal show={isOpened} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{currentModal.modalTitle}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <currentModal.modalBodyComponent {...currentModal.modalProps} />
+      </Modal.Body>
+    </Modal>
+  );
 };
 
-const Modal = () => (
-  <ModalContents />
-);
-
-export default Modal;
+export default ModalContents;
